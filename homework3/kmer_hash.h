d6495a5309eb9d5973c900756e4c9ab447824f70
Matthew French <tg832664@login1.stampede.tacc.utexas.edu>
2016-03-29 15:26:25 -0500

    Fixed some small problems with the code

--- homework3/kmer_hash.h
+++ homework3/kmer_hash.h
@@ -38,7 +38,8 @@ int64_t hashseq(int64_t  hashtable_size, char *seq, int size)
 {
    unsigned long hashval;
    hashval = 5381;
-   for(int i = 0; i < size; i++) {
+   int i;
+   for(i = 0; i < size; i++) {
       hashval = seq[i] +  (hashval << 5) + hashval;
    }
    
